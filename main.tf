68fdaed4833dcc925a26b93c269ec34a77d7d951
Nouven ltd <sandeep.baliyan@live.in>
2024-09-28 23:05:24 +1200

    remove taregt

--- main.tf
+++ main.tf
@@ -69,12 +69,7 @@ module "blog_alb" {
       backend_port     = 80
       target_type      = "instance"
     }
-    targets = {
-      my_target = {
-        target_id = aws.instance.blog.id 
-        port = 80
-      }
-    }
+    
   ]
 
   http_tcp_listeners = [

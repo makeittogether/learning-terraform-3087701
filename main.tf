101c8c54429b59a25210669507540dedbe6a8401
Nouven ltd <sandeep.baliyan@live.in>
2024-09-28 18:25:30 +1200

    removing custom SG

--- main.tf
+++ main.tf
@@ -46,20 +46,3 @@ module "blog_sg" {
 
 
 
-resource "aws_security_group" "blog" {
-  name = "blog"
-  description = "allow http and https and allow everything out"
-
-  vpc_id = data.aws_vpc.default.id
-}
-
-resource "aws_security_group_rule" "blog_http_in" {
-  type = "ingress"
-  from_port = 80
-  to_port = 80
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.blog.id
-}
-
-
